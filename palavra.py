2895e8e4f09f9ba0bbdcb5b261f67d41fbdabc0f
Josué Auler Kreischer <josueauler1606@gmail.com>
2025-08-31 12:11:38 -0300

    Update palavra.py

--- palavra.py
+++ palavra.py
@@ -38,16 +38,16 @@ driver.minimize_window()
 sleep(1)
 buscar= driver.find_element(By.NAME, "Search")
 botao= driver.find_element(By.XPATH, "//button[@class='relative items-center font-bold ease-in-out duration-100 focus:outline-2 focus:outline-info-light dark:focus:outline-info-dark hover:shadow-light-2 disabled:text-gray-50 dark:disabled:bg-gray-40 dark:disabled:text-white disabled:hover:shadow-none disabled:opacity-50 disabled:bg-gray-10 disabled:cursor-not-allowed max-w-fit dark:bg-yv-red-dark text-white text-xs px-3 inline-flex bg-yv-red rounded-full cursor-pointer h-4 w-4 justify-center mli-1 mlb-1 pli-0.5 z-base']")
-buscar.send_keys(escolhido) #pesquisa o livro
+buscar.send_keys(escolhido)
 botao.click()
 sleep(1)
-versiculos= driver.find_elements(By.XPATH, "//p[@class]") # retorna versiculos desse livro
+versiculos= driver.find_elements(By.XPATH, "//p[@class]")
 link= driver.find_elements(By.XPATH, "//a[@class]")
 
-versiram= random.choice(versiculos[0:5]) #escolhe versiculos do indice 0 ao 3
+versiram= random.choice(versiculos[0:5])
 
 versiculo= versiram
-print('A palavra do dia é: {}\n{}'.format(link[10].text,versiculo.text))
+print('A palavra do dia é:\n{}'.format(versiculo.text))
 
 completo= int(input('DIGITE 1 PARA LER O VERSÍCULO COMPLETO OU 2 PARA FINALIZAR:\n'))
 if completo == 1:
